fc535d184602c653506e1ed0715d6bda37eb8669
Md Somad <mdsomad10@gmail.com>
2022-07-16 16:06:06 +0530

    RadioButton complit

--- lib/radio_button.dart
+++ lib/radio_button.dart
@@ -3,13 +3,16 @@ import 'package:flutter/material.dart';
 
 
 class RadioButton extends StatefulWidget {
-  const RadioButton({ Key? key }) : super(key: key);
 
   @override
   State<RadioButton> createState() => _RadioButtonState();
 }
 
 class _RadioButtonState extends State<RadioButton> {
+
+  Color _bulpColor = Colors.black;
+  var colorGroupValue;
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,11 +20,57 @@ class _RadioButtonState extends State<RadioButton> {
         title: Text("Custom Switches Uses"),
       ),
       body: Container(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-            
-      ]),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           Icon(Icons.lightbulb_rounded,size: 150,
+                    color: _bulpColor,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                width: 150,
+                child: Row(children: [
+                  Radio(
+                    activeColor: Colors.purple,
+                    value: 'Red', groupValue:colorGroupValue , onChanged: (value){
+                      setState(() {
+                      colorGroupValue = value;
+                      _bulpColor = Colors.red;
+                    });
+                }),
+                Text("Green Color")
+                ],)
+              ),
+              Container(
+                width: 150,
+                child: Row(children: [
+                  Radio(value: 'green', groupValue:colorGroupValue , onChanged: (value){
+                      setState(() {
+                      colorGroupValue = value;
+                      _bulpColor = Colors.green;
+                    });
+                }),
+                Text("Green Color")
+                ],)
+              ),
+              Container(
+                width: 150,
+                child: Row(children: [
+                  Radio(value: 'Blue', groupValue:colorGroupValue , onChanged: (value){
+                    setState(() {
+                      colorGroupValue = value;
+                      _bulpColor = Colors.blue;
+                    });
+                }),
+                Text("Blue Color")
+                ],)
+              ),
+              
+        ]),
+      ),
     )
     );
   }
