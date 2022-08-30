2d541284035231268d9830850f35201810ecc968
Md Somad <mdsomad10@gmail.com>
2022-08-30 20:47:24 +0530

    complite

--- lib/main.dart
+++ lib/main.dart
@@ -5,50 +5,77 @@ void main() => runApp(const MyApp());
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  static const String _title = 'Flutter Code Sample';
+  static const String _title = 'RefreshIndicator Sample';
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: _title,
-      home: Scaffold(
-        appBar: AppBar(title: const Text(_title)),
-        body: const Center(
-          child: MyStatelessWidget(),
+    
+      home: RefreshIndicatorExample(title: _title),
+      
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+class RefreshIndicatorExample extends StatefulWidget {
+  const RefreshIndicatorExample({Key? key, required this.title})
+      : super(key: key);
+
+  final String title;
+
+  @override
+  State<RefreshIndicatorExample> createState() =>
+      _RefreshIndicatorExampleState();
+}
+
+class _RefreshIndicatorExampleState extends State<RefreshIndicatorExample> {
+
+  final GlobalKey<RefreshIndicatorState> _refreshIndicatorKey =
+      GlobalKey<RefreshIndicatorState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: RefreshIndicator(
+        key: _refreshIndicatorKey,
+        color: Colors.white,
+        backgroundColor: Colors.pink,
+        strokeWidth: 4.0,
+        onRefresh: () async {
+          // Replace this delay with the code to be executed during refresh
+          // and return a Future when code finishs execution.
+          return Future<void>.delayed(const Duration(seconds: 3));
+        },
+        // Pull from top to show refresh indicator.
+        child: ListView.builder(
+          itemCount: 25,
+          itemBuilder: (BuildContext context, int index) {
+            return ListTile(
+              title: Text('Item $index'),
+            );
+          },
         ),
       ),
-    );
-  }
-}
-
-
-
-
-
-class MyStatelessWidget extends StatelessWidget {
-  const MyStatelessWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 400,
-      width: 300,
-      color: Colors.red,
-      child: FittedBox(
-        fit: BoxFit.fill,
-        child: Image.network(
-            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg'),
+      floatingActionButton: FloatingActionButton.extended(
+        onPressed: () {
+          // Show refresh indicator programmatically on button tap.
+          _refreshIndicatorKey.currentState?.show();
+        },
+        icon: const Icon(Icons.refresh),
+        label: const Text('Show Indicator'),
       ),
     );
   }
 }
-
-
-
-
-
-
-
-
-
-
