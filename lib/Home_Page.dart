457e3199a287912cd54c6f5baa4445419da8daeb
Md Somad <mdsomad10@gmail.com>
2022-11-14 13:21:33 +0530

    complite

--- lib/Home_Page.dart
+++ lib/Home_Page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_widgets/drwer.dart';
-import 'package:flutter_widgets/provider/home_page_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:intl/intl.dart';
+import 'package:timeago/timeago.dart' as tago;
+
 
 
 
@@ -13,33 +13,135 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  
+   late DateTime dateTime = DateTime.now();
+
+
+
+
+
+
+String timeAgo(DateTime d) {                          // <-- Time Show Method
+ Duration diff = DateTime.now().difference(d);
+ if (diff.inDays > 365)
+  return "${(diff.inDays / 365).floor()} ${(diff.inDays / 365).floor() == 1 ? "year" : "years"} ago";
+ if (diff.inDays > 30)
+  return "${(diff.inDays / 30).floor()} ${(diff.inDays / 30).floor() == 1 ? "month" : "months"} ago";
+ if (diff.inDays > 7)
+  return "${(diff.inDays / 7).floor()} ${(diff.inDays / 7).floor() == 1 ? "week" : "weeks"} ago";
+ if (diff.inDays > 0)
+  return "${diff.inDays} ${diff.inDays == 1 ? "day" : "days"} ago";
+ if (diff.inHours > 0)
+  return "${diff.inHours} ${diff.inHours == 1 ? "hour" : "hours"} ago";
+ if (diff.inMinutes > 0)
+  return "${diff.inMinutes} ${diff.inMinutes == 1 ? "minute" : "minutes"} ago";
+ return "just now";
+}
+
+
+
+
+
+
+
+String getVerboseDateTimeRepresentation(DateTime dateTime) {    // Method 2 Time Dislay 
+    DateTime now = DateTime.now().toLocal();
+
+    DateTime localDateTime = dateTime.toLocal();
+
+    if (localDateTime.difference(now).inDays == 0) {
+      var differenceInHours = localDateTime.difference(now).inHours.abs();
+      var differenceInMins = localDateTime.difference(now).inMinutes.abs();
+
+      if (differenceInHours > 0) {
+        return '$differenceInHours hours ago';
+      } else if (differenceInMins > 2) {
+        return '$differenceInMins mins ago';
+      } else {
+        return 'Just now';
+      }
+    }
+
+    String roughTimeString = DateFormat('jm').format(dateTime);
+
+    if (localDateTime.day == now.day &&
+        localDateTime.month == now.month &&
+        localDateTime.year == now.year) {
+      return roughTimeString;
+    }
+
+    DateTime yesterday = now.subtract(const Duration(days: 1));
+
+    if (localDateTime.day == yesterday.day &&
+        localDateTime.month == now.month &&
+        localDateTime.year == now.year) {
+      return 'Yesterday';
+    }
+
+    if (now.difference(localDateTime).inDays < 4) {
+      String weekday = DateFormat(
+        'EEEE',
+      ).format(localDateTime);
+
+      return '$weekday, $roughTimeString';
+    }
+
+    return '${DateFormat('yMd').format(dateTime)}, $roughTimeString';
+  }
+
+
+
 
 
   
   @override
   Widget build(BuildContext context) {
-    
-    
     return Scaffold(
       appBar: AppBar(
-        title: Text('Dark Theme or light using'),
+        title: Text('DateTime Display using'),
       ),
 
-      drawer: DrawerPage(),
-      
-      body: Consumer<themeProvider>(
-        builder: (context, value, child) {
-          return Center(
-        child: Column(
+      body: Container(
+        child:Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-             Text('hello',style: TextStyle(
-              color: Theme.of(context).primaryColor
-             )
-             )
+
+            Text(DateFormat.yMEd().add_jms().format(dateTime)),     //<-- DateTime Show Package use -->  intl: ^0.17.0  (Display Example  ==> 'Thu, 5/23/2013 10:21:47 AM')
+
+         
+          SizedBox(
+            height: 10,
+          ),
+
+
+            Text(tago.format(dateTime)),                           //<-- DateTime Show Package use -->  timeago: ^3.3.0  (Display Example  ==> 'a moment ago')
+
+
+          SizedBox(
+            height: 10,
+          ),
+              
+
+            Text(timeAgo(dateTime)),                                    // <-- DateTime use call timeAgo Method  (Display Example  ==> 'just nown  )
+
+
+          SizedBox(
+            height: 10,
+          ),
+            
+            
+            Text(getVerboseDateTimeRepresentation(dateTime)),        //  <-- call Method 2 --> getVerboseDateTimeRepresentation Time display                                 // <-- DateTime Show Package use --> timeAgo Method  (Display Example  ==> 'just nown  )
+            
+            
+             Center(child: TextButton(onPressed: (){
+              setState(() {
+                dateTime = DateTime.now();
+              });
+             }, child: Text("Tap")))
           ],
-        ),
-      );
-      },)
+        ) ,
+      )
 
 
 
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:flutter/material.dart';
 import 'package:flutter_widgets/Home_Page.dart';
-import 'package:flutter_widgets/provider/home_page_provider.dart';
-import 'package:flutter_widgets/services/localStorage.dart';
-import 'package:flutter_widgets/services/themes.dart';
-import 'package:provider/provider.dart';
+
 
 
 
@@ -12,37 +9,21 @@ import 'package:provider/provider.dart';
 
 
 void main()async {
-  WidgetsFlutterBinding.ensureInitialized();
-  String currentTheme = await localStorage.getTheme() ?? "light"; 
-  runApp(MyApp(theme: currentTheme,));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
- final String theme;
-  const MyApp({ Key? key ,required this.theme}) : super(key: key);
+  const MyApp({ Key? key ,}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(providers: [
-
-        ChangeNotifierProvider<themeProvider>(
-          create:(context) => themeProvider(theme),
-          )
-    ],
-
-    child: Consumer<themeProvider>(builder: (context, value, child) {
       return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: "Flutter Widgets",
-      themeMode:value.themeMode,
-      theme: lightTheme,
-      
-     darkTheme: darkTheme,
 
      home:HomePage() ,
 
-    );
-    },),
-    );
-  }
+       );
+    }
+
 }
